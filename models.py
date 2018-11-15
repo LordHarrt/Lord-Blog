3730c5b4f7bf4d9bd9ad9335d8d5b83d97b325c9
anyidong <anyidong1993@163.com>
2018-11-15 09:59:04 +0800

    Update models.py

--- models.py
+++ models.py
@@ -13,6 +13,11 @@ class User(db.Model):
     id = db.Column(db.String(45), primary_key=True)
     username = db.Column(db.String(255))
     password = db.Column(db.String(255))
+    # Establish contact with Post's ForeignKey: user_id
+    posts = db.relationship(
+        "Post",
+        backref="users",
+        lazy="dynamic")
 
     def __init__(self, id, username, password):
         self.id = id
@@ -23,3 +28,27 @@ class User(db.Model):
     def __repr__(self):
         """Define the string format for instance of User"""
         return "<Model User `{}`>".format(self.username)
+
+
+class Post(db.Model):
+    """Represents Proected posts."""
+
+    __tablename__ = "posts"
+    id = db.Column(db.String(45), primary_key=True)
+    title = db.Column(db.String(255))
+    text = db.Column(db.Text())
+    publish_date = db.Column(db.DateTime)
+    # set the foreign key for Post
+    user_id = db.Column(db.String(45), db.ForeignKey("users.id"))
+
+    def __init__(self, title):
+        self.title = title
+
+    def __repr__(self):
+        return "<Model Post `{}`>".format(self.title)
+
+
+
+
+
+
